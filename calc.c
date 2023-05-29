06d1df94fb3e7ed73e631901336f8e8dbdc0532d
kpavan11 <77949226+kpavan11@users.noreply.github.com>
2023-05-29 18:34:38 +0530

    Create calc.c

---
+++ calc.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+int result=0, num1,num2;
+char opt;
+int main()
+{
+while(1)
+{
+    if((opt=='q') || (opt=='Q'))
+    {
+        break;
+     }
+    printf("enter two integers and operator\n");
+    scanf("%d %d %c",&num1,&num2,&opt);
+    
+    switch(opt)
+    {
+    case '+' :
+               result = num1 + num2;
+               break;
+    case '-' :
+               result = num1 - num2;
+               break;
+    case '*' :
+               result = num1 * num2;
+               break;
+    case '%' :
+               result = num1 % num2;
+               break;
+    case '/' : 
+                if(num2 == 0)
+                {
+                  printf("Error:Divide by zero\n");
+                  continue;
+                  }
+                else
+                {
+                result = num1 / num2;
+                }
+                break;
+    default :
+             printf("cheak operator\n");
+             result = 0;
+             break;
+   }
+   printf("Result is: %d\n",result);
+
+ }
+ return 0;
+}   
